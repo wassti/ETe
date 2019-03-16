e6269bf1a6fe495a5470eb2ad6a46f804c51afe6
Ensiform <ensiform@gmail.com>
2019-03-15 19:05:06 -0500

    Fix last commit on *nix

--- src/renderer/tr_model.c
+++ src/renderer/tr_model.c
@@ -2339,12 +2339,12 @@ void *R_Hunk_Begin( void ) {
 		// show_bug.cgi?id=440
 		// if not win32, then just allocate it now
 		// it is possible that we have been allocated already, in case we don't do anything
-		membase = malloc( maxsize );
+		membase = malloc( R_HUNK_SIZE );
 		// TTimo NOTE: initially, I was doing the memset even if we had an existing membase
 		// but this breaks some shaders (i.e. /map mp_beach, then go back to the main menu .. some shaders are missing)
 		// I assume the shader missing is because we don't clear memory either on win32
 		// meaning even on win32 we are using memory that is still reserved but was uncommited .. it works out of pure luck
-		memset( membase, 0, maxsize );
+		memset( membase, 0, R_HUNK_SIZE );
 #endif
 	}
 

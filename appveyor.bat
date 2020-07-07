90b0aab2f41d4f8d26482101efbfc8fcf47a09ef
Ensiform <ensiform@gmail.com>
2020-07-07 17:54:51 -0500

    Update travis and appveyor zip output name

--- appveyor.bat
+++ appveyor.bat
@@ -4,12 +4,17 @@ msbuild "wolf.sln" /m /p:PlatformToolset="v141_xp" /p:Configuration="%BUILD_CONF
 
 IF ERRORLEVEL 1 GOTO ERROR
 
+set buildyear=%date:~10,4%
+set buildmonth=%date:~4,2%
+set buildday=%date:~7,2%
+set build_date=%buildyear%%buildmonth%%buildday%
+
 cd %APPVEYOR_BUILD_FOLDER%/src/%BUILD_CONFIGURATION%
-7z a "%APPVEYOR_BUILD_FOLDER%/ETe-Win32-%APPVEYOR_REPO_TAG_NAME%-%BUILD_CONFIGURATION%.7z" ETe.exe
+7z a "%APPVEYOR_BUILD_FOLDER%/wolfet-2.60e-%build_date%-%BUILD_CONFIGURATION%-win32.zip" ETe.exe
 cd %APPVEYOR_BUILD_FOLDER%/src/ded/%BUILD_CONFIGURATION%
-7z a "%APPVEYOR_BUILD_FOLDER%/ETe-Win32-%APPVEYOR_REPO_TAG_NAME%-%BUILD_CONFIGURATION%.7z" ETe-ded.exe
+7z a "%APPVEYOR_BUILD_FOLDER%/wolfet-2.60e-%build_date%-%BUILD_CONFIGURATION%-win32.zip" ETe.ded.exe
 cd %APPVEYOR_BUILD_FOLDER%
-7z a "ETe-Win32-%APPVEYOR_REPO_TAG_NAME%-%BUILD_CONFIGURATION%.7z" docs\*
+7z a "%APPVEYOR_BUILD_FOLDER%/wolfet-2.60e-%build_date%-%BUILD_CONFIGURATION%-win32.zip" docs\*
 
 IF ERRORLEVEL 1 GOTO ERROR
 

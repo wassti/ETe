80f95bee506bbfda9369c362c23400daac17e9cc
Ensiform <ensiform@gmail.com>
2020-08-03 09:48:36 -0500

    Apple binary name changes (won't be enough to get running)
    ET has deeper/enhanced mac support that will be missing from our current codepath.
    
    From https://github.com/ec-/Quake3e/commit/d684d53bd7baa3df3f60080c2a1c48ac3384dec8

--- src/unix/unix_main.c
+++ src/unix/unix_main.c
@@ -1229,28 +1229,35 @@ builds because there are situations where you are likely to want
 to symlink to binaries and /not/ have the links resolved.
 =================
 */
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
 const char *Sys_BinName( const char *arg0 )
 {
-	static char   dst[ PATH_MAX ];
+	static char dst[ PATH_MAX ];
 
 #ifdef NDEBUG
 
-#ifdef __linux__
+#if defined (__linux__)
 	int n = readlink( "/proc/self/exe", dst, PATH_MAX - 1 );
 
 	if ( n >= 0 && n < PATH_MAX )
 		dst[ n ] = '\0';
 	else
 		Q_strncpyz( dst, arg0, PATH_MAX );
+#elif defined (__APPLE__)
+	uint32_t bufsize = sizeof( dst );
+
+	_NSGetExecutablePath( dst, &bufsize );
 #else
+
 #warning Sys_BinName not implemented
 	Q_strncpyz( dst, arg0, PATH_MAX );
 #endif
 
-#else
+#else // DEBUG
 	Q_strncpyz( dst, arg0, PATH_MAX );
 #endif
-
 	return dst;
 }
 

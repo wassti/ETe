43a0642fa051b918e87e703ebcadab994f208271
Ensiform <ensiform@gmail.com>
2020-08-03 09:42:04 -0500

    Context changes for renderer shutdown (generally used by Vulkan)
    From https://github.com/ec-/Quake3e/commit/f893c2715b4562bbc872050b828f7dcb7aa27a99

--- src/renderer/tr_init.c
+++ src/renderer/tr_init.c
@@ -1943,9 +1943,9 @@ void R_PurgeCache( void ) {
 RE_Shutdown
 ===============
 */
-static void RE_Shutdown( int destroyWindow ) {
+static void RE_Shutdown( refShutdownCode_t code ) {
 
-	ri.Printf( PRINT_ALL, "RE_Shutdown( %i )\n", destroyWindow );
+	ri.Printf( PRINT_ALL, "RE_Shutdown( %i )\n", code );
 
 	ri.Cmd_RemoveCommand( "modellist" );
 	ri.Cmd_RemoveCommand( "screenshotBMP" );
@@ -1968,7 +1968,7 @@ static void RE_Shutdown( int destroyWindow ) {
 
 	if ( r_cache->integer ) {
 		if ( tr.registered ) {
-			if ( destroyWindow ) {
+			if ( code != REF_KEEP_CONTEXT ) {
 				//R_IssuePendingRenderCommands();
 				R_DeleteTextures();
 			} else {
@@ -1987,13 +1987,13 @@ static void RE_Shutdown( int destroyWindow ) {
 	R_DoneFreeType();
 
 	// shut down platform specific OpenGL stuff
-	if ( destroyWindow ) {
+	if ( code != REF_KEEP_CONTEXT ) {
 
 		QGL_DoneARB();
 
 		VBO_Cleanup();
 
-		ri.GLimp_Shutdown( destroyWindow == 2 ? qtrue: qfalse );
+		ri.GLimp_Shutdown( code == REF_UNLOAD_DLL ? qtrue: qfalse );
 
 		R_ClearSymTables();
 

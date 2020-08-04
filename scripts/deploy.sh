2e75ea72aa8b586b1feded479f464ec9f6c5f95a
Tuomo Sirén <tuomo.siren@gmail.com>
2020-08-05 00:45:29 +0300

    Try without heredoc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,16 +13,15 @@ ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
 file_count=$(ssh $USER@$HOST ls $SNAPSHOT_DIR | wc -l)
 if (( file_count > MAX_FILE_HISTORY )); then
     remove_count=$((file_count - MAX_FILE_HISTORY))
-    ssh -T $USER@$HOST << EOSSH
-rm "$(ls -t $SNAPSHOT_DIR | tail -$remove_count)"
-EOSSH
+    files_to_remove=$(ssh $USER@$HOST ls -t $SNAPSHOT_DIR | tail -$remove_count)
+    ssh $USER@$HOST cd $SNAPSHOT_DIR && rm $files_to_remove
 fi
 
-ssh etf@etf2.org mkdir -p $TMP_DIR
-scp $TRAVIS_BUILD_DIR/*.7z etf@etf2.org:$TMP_DIR
-ssh etf@etf2.org 7z x /tmp/etfdeploy/*.7z -aoa -o/home/etf/wolfet
-ssh etf@etf2.org mv $TMP_DIR/* $SNAPSHOT_DIR
-ssh etf@etf2.org rm -rf $TMP_DIR
-ssh etf@etf2.org sudo systemctl restart etf.service
+ssh $USER@$HOST mkdir -p $TMP_DIR
+scp $TRAVIS_BUILD_DIR/*.7z $USER@$HOST:$TMP_DIR
+ssh $USER@$HOST 7z x /tmp/etfdeploy/*.7z -aoa -o/home/etf/wolfet
+ssh $USER@$HOST mv $TMP_DIR/* $SNAPSHOT_DIR
+ssh $USER@$HOST rm -rf $TMP_DIR
+ssh $USER@$HOST sudo systemctl restart etf.service
 
 echo "Deployment successful"

b8f68cfa03d78a93aebe8d473182be56508482c9
Tuomo Sirén <tuomo.siren@gmail.com>
2020-08-05 01:07:43 +0300

    Dont add win batch files from docs to linux archive

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,23 +5,27 @@ set -euxo
 MAX_FILE_HISTORY=10
 SNAPSHOT_DIR=/home/etf/www_files/ete-linux-i386
 TMP_DIR=/tmp/etfdeploy
+DEPLOY_DIR=/home/etf/wolfet
 USER=etf
 HOST=etf2.org
+SERVICE_NAME=etf.service
 
 ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
 
 file_count=$(ssh $USER@$HOST ls $SNAPSHOT_DIR | wc -l)
 if (( file_count > MAX_FILE_HISTORY )); then
-    remove_count=$((file_count - MAX_FILE_HISTORY))
-    files_to_remove=$(ssh $USER@$HOST ls -t $SNAPSHOT_DIR | tail -$remove_count)
-    ssh $USER@$HOST cd $SNAPSHOT_DIR && rm $files_to_remove
+    remove_count=$((file_count - MAX_FILE_HISTORY + 1))
+    files_to_remove=$(ssh $USER@$HOST ls -t $SNAPSHOT_DIR | tail -$remove_count | tr '\n' ' ')
+    # remove last whitespace
+    files_to_remove=${files_to_remove%?}
+    ssh $USER@$HOST "cd $SNAPSHOT_DIR && rm $files_to_remove"
 fi
 
 ssh $USER@$HOST mkdir -p $TMP_DIR
 scp $TRAVIS_BUILD_DIR/*.7z $USER@$HOST:$TMP_DIR
-ssh $USER@$HOST 7z x /tmp/etfdeploy/*.7z -aoa -o/home/etf/wolfet
+ssh $USER@$HOST 7z x $TMP_DIR/*.7z -aoa -o$DEPLOY_DIR
 ssh $USER@$HOST mv $TMP_DIR/* $SNAPSHOT_DIR
 ssh $USER@$HOST rm -rf $TMP_DIR
-ssh $USER@$HOST sudo systemctl restart etf.service
+ssh $USER@$HOST sudo systemctl restart $SERVICE_NAME
 
 echo "Deployment successful"

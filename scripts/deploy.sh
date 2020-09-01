cb6a78ba6ab80ce3354c1b7b44f5a8f0697d8525
Tuomo Sirén <tuomo.siren@gmail.com>
2020-09-02 02:07:48 +0300

    Add win snapshot deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,8 @@
 set -euxo
 
 MAX_FILE_HISTORY=10
-SNAPSHOT_DIR=/home/etf/www_files/ete-linux-i386
+LINUX_SNAPSHOT_DIR=/home/etf/www_files/ete-linux-i386
+WIN_SNAPSHOT_DIR=/home/etf/www_files/ete-win32
 TMP_DIR=/tmp/etfdeploy
 DEPLOY_DIR=/home/etf/wolfet
 USER=etf
@@ -13,21 +14,24 @@ SERVICE_NAME=etf.service
 ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
 
 ssh $USER@$HOST bash <<EOF
-file_count=\$(ls $SNAPSHOT_DIR | wc -l)
-if (( \$file_count > $MAX_FILE_HISTORY )); then
-    remove_count=\$((file_count - MAX_FILE_HISTORY + 1))
-    files_to_remove=\$(ls -t $SNAPSHOT_DIR | tail -\$remove_count | tr '\n' ' ')
-    # remove last whitespace
-    files_to_remove=\${files_to_remove%?}
-    cd $SNAPSHOT_DIR && rm \$files_to_remove
-fi
+function clean_snapshots {
+    file_count=\$(ls $1 | wc -l)
+    if (( \$file_count > $MAX_FILE_HISTORY )); then
+        remove_count=\$((file_count - MAX_FILE_HISTORY + 1))
+        files_to_remove=\$(ls -t $1 | tail -\$remove_count | tr '\n' ' ')
+        # remove last whitespace
+        files_to_remove=\${files_to_remove%?}
+        cd $1 && rm \$files_to_remove
+    fi
+}
+clean_snapshots $LINUX_SNAPSHOT_DIR
+clean_snapshots $WIN_SNAPSHOT_DIR
 EOF
-
 
 ssh $USER@$HOST mkdir -p $TMP_DIR
 scp $TRAVIS_BUILD_DIR/*.7z $USER@$HOST:$TMP_DIR
 ssh $USER@$HOST 7z x $TMP_DIR/*.7z -aoa -o$DEPLOY_DIR
-ssh $USER@$HOST mv $TMP_DIR/* $SNAPSHOT_DIR
+ssh $USER@$HOST mv $TMP_DIR/* $LINUX_SNAPSHOT_DIR
 ssh $USER@$HOST rm -rf $TMP_DIR
 ssh $USER@$HOST sudo systemctl restart $SERVICE_NAME
 

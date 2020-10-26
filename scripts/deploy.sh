b154abe2898508818a2b24df29a67a0571211c1f
Tuomo Sirén <tuomo.siren@gmail.com>
2020-10-26 23:06:58 +0200

    Use server crontab for deleting older deployment snapshots

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,7 @@
 
 set -euxo
 
-MAX_FILE_HISTORY=10
 LINUX_SNAPSHOT_DIR=/home/etf/www_files/ete-linux-i386
-WIN_SNAPSHOT_DIR=/home/etf/www_files/ete-win32
 TMP_DIR=/tmp/etfdeploy
 DEPLOY_DIR=/home/etf/wolfet
 USER=etf
@@ -12,22 +10,6 @@ HOST=etf2.org
 SERVICE_NAME=etf.service
 
 ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
-
-ssh $USER@$HOST bash <<EOF
-set -euxo
-function clean_snapshots {
-    file_count=\$(ls \$1 | wc -l)
-    if (( \$file_count > $MAX_FILE_HISTORY )); then
-        remove_count=\$((file_count - $MAX_FILE_HISTORY + 1))
-        files_to_remove=\$(ls -t \$1 | tail -\$remove_count | tr '\n' ' ')
-        # remove last whitespace
-        files_to_remove=\${files_to_remove%?}
-        cd \$1 && rm \$files_to_remove
-    fi
-}
-clean_snapshots $LINUX_SNAPSHOT_DIR
-clean_snapshots $WIN_SNAPSHOT_DIR
-EOF
 
 ssh $USER@$HOST mkdir -p $TMP_DIR
 scp $TRAVIS_BUILD_DIR/*.7z $USER@$HOST:$TMP_DIR

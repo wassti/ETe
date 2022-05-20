ac22708962bf2cb0d61f13f5ea450f00bc28aa61
Ensiform <ensiform@gmail.com>
2022-05-19 20:36:16 -0500

    Use scancodes for SDL input
    Merge of https://github.com/ec-/Quake3e/commit/3c210113df8520b3ccd92a07c9d7e6a9c0411835
    
    Check in_forceCharset for SDL backend
    Merge of https://github.com/ec-/Quake3e/commit/ff93e3ed7b713408d5ce5af13eaea5cfc275e6af
    
    See https://github.com/ec-/Quake3e/pull/165

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "sdl_glw.h"
 
 static cvar_t *in_keyboardDebug;
+static cvar_t *in_forceCharset;
 
 #ifdef USE_JOYSTICK
 static SDL_GameController *gamepad;
@@ -228,7 +229,7 @@ static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
 {
 	keyNum_t key = 0;
 
-	/*if ( keysym->scancode >= SDL_SCANCODE_1 && keysym->scancode <= SDL_SCANCODE_0 )
+	if ( keysym->scancode >= SDL_SCANCODE_1 && keysym->scancode <= SDL_SCANCODE_0 )
 	{
 		// Always map the number keys as such even if they actually map
 		// to other characters (eg, "1" is "&" on an AZERTY keyboard).
@@ -239,12 +240,40 @@ static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
 		else
 			key = '1' + keysym->scancode - SDL_SCANCODE_1;
 	}
-	else */if( keysym->sym >= SDLK_SPACE && keysym->sym < SDLK_DELETE )
+	else if ( in_forceCharset->integer > 0 )
+	{
+		if ( keysym->scancode >= SDL_SCANCODE_A && keysym->scancode <= SDL_SCANCODE_Z )
+		{
+			key = 'a' + keysym->scancode - SDL_SCANCODE_A;
+		}
+		else
+		{
+			switch ( keysym->scancode )
+			{
+				case SDL_SCANCODE_MINUS:        key = '-';  break;
+				case SDL_SCANCODE_EQUALS:       key = '=';  break;
+				case SDL_SCANCODE_LEFTBRACKET:  key = '[';  break;
+				case SDL_SCANCODE_RIGHTBRACKET: key = ']';  break;
+				case SDL_SCANCODE_NONUSBACKSLASH:
+				case SDL_SCANCODE_BACKSLASH:    key = '\\'; break;
+				case SDL_SCANCODE_SEMICOLON:    key = ';';  break;
+				case SDL_SCANCODE_APOSTROPHE:   key = '\''; break;
+				case SDL_SCANCODE_COMMA:        key = ',';  break;
+				case SDL_SCANCODE_PERIOD:       key = '.';  break;
+				case SDL_SCANCODE_SLASH:        key = '/';  break;
+				default:
+					/* key = 0 */
+					break;
+			}
+		}
+	}
+
+	if( !key && keysym->sym >= SDLK_SPACE && keysym->sym < SDLK_DELETE )
 	{
 		// These happen to match the ASCII chars
 		key = (int)keysym->sym;
 	}
-	else
+	else if( !key )
 	{
 		switch( keysym->sym )
 		{
@@ -349,7 +378,7 @@ static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
 	if( in_keyboardDebug->integer )
 		IN_PrintKey( keysym, key, down );
 
-	if ( keysym->scancode == SDL_SCANCODE_GRAVE )
+	if ( !(keysym->mod & KMOD_CTRL) && keysym->scancode == SDL_SCANCODE_GRAVE )
 	{
 		SDL_Keycode translated = SDL_GetKeyFromScancode( SDL_SCANCODE_GRAVE );
 
@@ -359,7 +388,7 @@ static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
 			key = K_CONSOLE;
 		}
 	}
-	else if( IN_IsConsoleKey( key, 0 ) )
+	else if( !(keysym->mod & KMOD_CTRL) && IN_IsConsoleKey( key, 0 ) )
 	{
 		// Console keys can't be bound or generate characters
 		key = K_CONSOLE;
@@ -1359,6 +1388,7 @@ void IN_Init( void )
 	Com_DPrintf( "\n------- Input Initialization -------\n" );
 
 	in_keyboardDebug = Cvar_Get( "in_keyboardDebug", "0", CVAR_ARCHIVE );
+	in_forceCharset = Cvar_Get( "in_forceCharset", "1", CVAR_ARCHIVE_ND );
 
 	// mouse variables
 	in_mouse = Cvar_Get( "in_mouse", "1", CVAR_ARCHIVE );

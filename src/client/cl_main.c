5e0d4de76cb36d38fabc1ab3aabb238bc3a2505b
Ensiform <ensiform@gmail.com>
2022-05-31 21:06:56 -0500

    video-pipe: use full (pc) color space and do not enforce
    particular rate for output audio stream
    
    Merge of https://github.com/ec-/Quake3e/commit/7d79137eef78964b71544bf6d9224b3af748a970

--- src/client/cl_main.c
+++ src/client/cl_main.c
@@ -4470,8 +4470,8 @@ void CL_Init( void ) {
 	Cvar_CheckRange( cl_forceavidemo, "0", "1", CV_INTEGER );
 
 	cl_aviPipeFormat = Cvar_Get( "cl_aviPipeFormat",
-		"-preset medium -crf 23 -vcodec libx264 -flags +cgop -pix_fmt yuv420p "
-		"-bf 2 -codec:a aac -strict -2 -b:a 160k -r:a 22050 -movflags faststart",
+		"-preset medium -crf 23 -vcodec libx264 -flags +cgop -pix_fmt yuvj420p "
+		"-bf 2 -codec:a aac -strict -2 -b:a 160k -movflags faststart",
 		CVAR_ARCHIVE );
 
 	rconAddress = Cvar_Get( "rconAddress", "", 0 );
